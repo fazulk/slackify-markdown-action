7595d06a4174f90d3c56ab06c40f52f48ad5d05c
Jeff Fasulkey <fazulk@gmail.com>
2024-05-01 13:56:49 -0700

    wip

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const slackifyMarkdown = require('slackify-markdown');
 try {
     const input = JSON.parse(core.getInput('text', { required: true }))
     // const sample = JSON.parse(JSON.stringify("### &nbsp;&nbsp;&nbsp;🐞 Bug Fixes\n\n- Ymls &nbsp;-&nbsp; by @fazulk [<samp>(95e64)</samp>](https://github.com/fazulk/slackify-markdown-action/commit/95e64d6)\n\n##### &nbsp;&nbsp;&nbsp;&nbsp;[View changes on GitHub](https://github.com/fazulk/slackify-markdown-action/compare/v1.0.3...v1.0.4)"))
-    const markdown = slackifyMarkdown(input);
+    const markdown = JSON.stringify(slackifyMarkdown(input));
     core.setOutput("text",  `"${markdown}"`);
     // fs.writeFile('output.txt', markdown, (err) => {
     //   if (err)
