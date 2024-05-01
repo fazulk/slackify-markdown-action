1bb2c39f1d1c7bc359c72ad69c2d3dd27e9b6700
Jeff Fasulkey <fazulk@gmail.com>
2024-05-01 13:44:10 -0700

    wip

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ try {
     const input = core.getInput('text', { required: true });
     // const sample = "### &nbsp;&nbsp;&nbsp;🐞 Bug Fixes\n\n- Prob not &nbsp;-&nbsp; by @fazulk [<samp>(4d1ff)</samp>](https://github.com/fazulk/bp/commit/4d1ff57)\n\n##### &nbsp;&nbsp;&nbsp;&nbsp;[View changes on GitHub](https://github.com/fazulk/bp/compare/v0.1.38...v0.1.39)"
     const markdown = slackifyMarkdown(input);
-    core.setOutput("text",  `"${markdown}"`);
+    core.setOutput("text",  `${markdown}`);
     // fs.writeFile('output.txt', mrkdwn, (err) => {
     //   if (err)
     //     throw err
