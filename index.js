f4c056b9ec2a9db39e94e31982d22e322641e6a9
Jeff Fasulkey <fazulk@gmail.com>
2024-05-01 13:18:24 -0700

    wip

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const slackifyMarkdown = require('slackify-markdown');
 try {
     const input = core.getInput('text', { required: true });
     const mrkdwn = slackifyMarkdown(input);
-    const output = mrkdwn.replace(/\r\n|\r|\n/g, "\\n");
-    core.setOutput("text", output);
+    // const output = mrkdwn.replace(/\r\n|\r|\n/g, "\\n");
+    core.setOutput("text", mrkdwn);
 } catch (error) {
     core.setFailed(error.message);
 }
