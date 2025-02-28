2927b641f4cbd693baeede558908762bdc2a15b9
Jeff Fasulkey <fazulk@gmail.com>
2025-02-28 09:33:53 -0800

    test

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 import { getInput, setFailed, setOutput } from '@actions/core'
 import slackifyMarkdown from 'slackify-markdown'
 
-function cleanString(str) {
-  str = str.replaceAll('<samp>', '')
-  str = str.replaceAll('</samp>', '')
-  return str
-}
-
 try {
   const input = getInput('text', { required: true })
 
-  const preservedEmojis = input.replace(/:([\w+-]+):/g, '{{EMOJI_$1}}')
+  // const preservedEmojis = input.replace(/:([\w+-]+):/g, '{{EMOJI_$1}}')
 
-  const mrkdwn = slackifyMarkdown(preservedEmojis)
+  const mrkdwn = slackifyMarkdown(input)
 
-  const restoredEmojis = mrkdwn.replace(/{{EMOJI_([\w+-]+)}}/g, ':$1:')
+  // const restoredEmojis = mrkdwn.replace(/{{EMOJI_([\w+-]+)}}/g, ':$1:')
 
-  const output = restoredEmojis.replace(/\r\n|\r|\n/g, '\n')
+  const output = mrkdwn.replace(/\r\n|\r|\n/g, '\n')
+
   setOutput('text', output)
 }
 catch (error) {
