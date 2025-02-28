2d56a1230ff0dffaa78a310d392cf5a268b3a62d
Jeff Fasulkey <fazulk@gmail.com>
2025-02-28 09:51:36 -0800

    test

--- index.js
+++ index.js
@@ -2,26 +2,31 @@ import { getInput, setFailed, setOutput } from '@actions/core'
 import slackifyMarkdown from 'slackify-markdown'
 
 try {
-  console.log('Action started - slackify-markdown-action')
   const input = getInput('text', { required: true })
-  const cleaned = input.replace(/\r\n|\r|\n/g, '\n')
-  console.log('Input received, length:', input ? input.length : 0)
-  console.log('First 100 chars of input:', input ? input.substring(0, 100) : 'empty')
+  console.log(`First 100 chars of input: ${JSON.stringify(input.substring(0, 100))}`)
 
-  // const preservedEmojis = input.replace(/:([\w+-]+):/g, '{{EMOJI_$1}}')
+  const mrkdwn = JSON.stringify(slackifyMarkdown(JSON.parse(input)))
 
-  const mrkdwn = slackifyMarkdown(cleaned)
-  console.log('Conversion completed, output length:', mrkdwn ? mrkdwn.length : 0)
-  console.log('First 100 chars of output:', mrkdwn ? mrkdwn.substring(0, 100) : 'empty')
+  const cleaned = mrkdwn.replace(/\r\n|\r|\n/g, '\n')
+  console.log(`Conversion completed, output length: ${cleaned.length}`)
+  console.log(`First 100 chars of output: ${JSON.stringify(cleaned.substring(0, 100))}`)
 
-  // const restoredEmojis = mrkdwn.replace(/{{EMOJI_([\w+-]+)}}/g, ':$1:')
-
-  // const output = mrkdwn.replace(/\r\n|\r|\n/g, '\n')
-
-  setOutput('text', mrkdwn)
-  console.log('Action completed successfully')
+  setOutput('text', cleaned)
 }
 catch (error) {
   console.error('Action failed with error:', error.message)
   setFailed(error.message)
 }
+
+// function cleanString(str) {
+//   str = str.replaceAll('<samp>', '')
+//   str = str.replaceAll('</samp>', '')
+//   return str
+// }
+
+// try {
+//   let input = getInput('text', { required: true })
+//   input = cleanString(input)
+//   input = JSON.parse(input)
+//   const markdown = JSON.stringify(slackifyMarkdown(input))
+//   setOutput('text', markdown)
