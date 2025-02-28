5ac4fd4b61d2fa52be5db193a52ddb5c7e51c273
Jeff Fasulkey <fazulk@gmail.com>
2025-02-28 12:17:19 -0800

    t

--- index.js
+++ index.js
@@ -9,47 +9,20 @@ try {
   let markdownContent = input
   let mrkdwn
 
-  // console.log('Approach 1: Using input directly')
-  // try {
-  //   mrkdwn = slackifyMarkdown(input)
-  //   console.log('Sample output 1:', mrkdwn.substring(0, 100))
-  // }
-  // catch (e) {
-  //   console.log('Approach 1 failed:', e.message)
-  // }
-
-  console.log('Approach 2: Trying to parse as JSON')
   try {
     const parsed = JSON.parse(input)
     mrkdwn = slackifyMarkdown(parsed)
-    console.log('Sample output 2:', mrkdwn.substring(0, 100))
     markdownContent = parsed // Use this if it works
-    console.log('type of markdownContent:', typeof markdownContent)
   }
   catch (e) {
     console.log('Approach 2 failed:', e.message)
   }
 
-  // console.log('Approach 3: Removing outer quotes if present')
-  // try {
-  //   // If input starts and ends with quotes, remove them
-  //   const unquoted = input.replace(/^"(.*)"$/, '$1')
-  //   mrkdwn = slackifyMarkdown(unquoted)
-  //   console.log('Sample output 3:', mrkdwn.substring(0, 100))
-  //   if (unquoted !== input)
-  //     markdownContent = unquoted // Use this if it's different and works
-  // }
-  // catch (e) {
-  //   console.log('Approach 3 failed:', e.message)
-  // }
-
   // Use the best approach based on the logs
   mrkdwn = slackifyMarkdown(markdownContent)
-  const cleaned = mrkdwn.replace(/\r\n|\r|\n/g, '\n')
-  console.log(`Conversion completed, output length: ${cleaned.length}`)
-  console.log(`First 100 chars of output: ${JSON.stringify(cleaned.substring(0, 100))}`)
+  // const cleaned = mrkdwn.replace(/\r\n|\r|\n/g, '\n')
 
-  setOutput('text', JSON.stringify(cleaned))
+  setOutput('text', JSON.stringify(mrkdwn))
 }
 catch (error) {
   console.error('Action failed with error:', error.message)
