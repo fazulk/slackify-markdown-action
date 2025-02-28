e6d699bb672915b8784dbac2a1e5e1b6bece67f4
Jeff Fasulkey <fazulk@gmail.com>
2025-02-28 09:43:01 -0800

    t

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ import slackifyMarkdown from 'slackify-markdown'
 try {
   console.log('Action started - slackify-markdown-action')
   const input = getInput('text', { required: true })
+  const cleaned = input.replace(/\r\n|\r|\n/g, '\n')
   console.log('Input received, length:', input ? input.length : 0)
   console.log('First 100 chars of input:', input ? input.substring(0, 100) : 'empty')
 
   // const preservedEmojis = input.replace(/:([\w+-]+):/g, '{{EMOJI_$1}}')
 
-  const mrkdwn = slackifyMarkdown(input)
+  const mrkdwn = slackifyMarkdown(cleaned)
   console.log('Conversion completed, output length:', mrkdwn ? mrkdwn.length : 0)
   console.log('First 100 chars of output:', mrkdwn ? mrkdwn.substring(0, 100) : 'empty')
 
   // const restoredEmojis = mrkdwn.replace(/{{EMOJI_([\w+-]+)}}/g, ':$1:')
 
-  const output = mrkdwn.replace(/\r\n|\r|\n/g, '\n')
+  // const output = mrkdwn.replace(/\r\n|\r|\n/g, '\n')
 
-  setOutput('text', output)
+  setOutput('text', mrkdwn)
   console.log('Action completed successfully')
 }
 catch (error) {
